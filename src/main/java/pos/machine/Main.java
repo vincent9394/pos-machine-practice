568374e94e198641244974e7fd45f74a8090c9fc
vincent <luk96515@gmail.com>
2021-10-19 22:42:48 +0800

    [POS system][Vincent] feat: add quantity counter

--- src/main/java/pos/machine/Main.java
+++ src/main/java/pos/machine/Main.java
@@ -1,37 +1,42 @@
 package pos.machine;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
+    public static List<String> barcodes = ItemDataLoader.loadBarcodes();
+    public static List<ItemInfo> itemInfos = ItemDataLoader.loadAllItemInfos();
+    public static List<ItemInfo> barcodesItem = new ArrayList<>();
     public static void main(String[] arg) {
-//        System.out.println("hello world");
 
+            printItem();
+        System.out.println(countItemQuantity(barcodesItem.get(0).getBarcode()));
 
-        List<String> barcodes = ItemDataLoader.loadBarcodes();
-        List<ItemInfo> itemInfos = ItemDataLoader.loadAllItemInfos();
-        List<ItemInfo> barcodesItem =new ArrayList<>() ;
-//        System.out.println(itemInfos.get(0).getName());
-//        System.out.println(itemInfos.get(0));
-//        System.out.println(itemInfos.size());
-
-
-        for (int i=0;i<barcodes.size();i++){
-
-           for(int j=0; j<itemInfos.size();j++){
-                if(barcodes.get(i).equals(itemInfos.get(j).getBarcode())){
-                    barcodesItem.add(itemInfos.get(j));
-
-                } ;
-
-            }
-
-
-            System.out.println(barcodesItem.get(i).getBarcode());
-            System.out.println(barcodesItem.get(i).getName());
-            System.out.println(barcodesItem.get(i).getPrice());
-       }
-
-        //ItemInfo itemInfo= new ItemInfo(a,b,c);
     }
+    public static void printItem(){
+
+        for (int i = 0; i < barcodes.size(); i++) {
+            for (int j = 0; j < itemInfos.size(); j++) {
+                if (barcodes.get(i).equals(itemInfos.get(j).getBarcode())) {
+                    barcodesItem.add(itemInfos.get(j));
+                }
+            }
+//            System.out.println(barcodesItem.get(i).getBarcode());
+//
+//            System.out.println(barcodesItem.get(i).getName());
+//            System.out.println(barcodesItem.get(i).getPrice());
+        }
+
+    }
+    public static int countItemQuantity(String s){
+        int count = 0;
+        for(int i=0;i<barcodes.size();i++){
+            if(s.equals(barcodes.get(i))){
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
